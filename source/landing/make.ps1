31d1e029c323897a02d6678fd508e159e2558fdf
Doug Slater <doug@slater.dev>
2023-08-06 23:05:23 -0400

    Use action cache

--- source/landing/make.ps1
+++ source/landing/make.ps1
@@ -1,5 +1,6 @@
 sudo gem install bundler
 sudo gem install jekyll minima jekyll-feed addressable terminal-table webrick jekyll-seo-tag rexml unicode-display_width
-bundle install
+bundle install --jobs 4 --retry 3
+bundle config path vendor/bundle
 bundle exec jekyll build
 & ./sync.ps1
